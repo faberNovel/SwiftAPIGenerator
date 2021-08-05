a62e5d9ac852bd0f3f55341bc9c0190dd18c73cc
Benjamin Lavialle <benjamin.lavialle@fabernovel.com>
2021-08-05 09:10:57 +0200

    Add dependencies

--- Package.swift
+++ Package.swift
@@ -6,15 +6,18 @@ import PackageDescription
 let package = Package(
     name: "SwiftAPIGenerator",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.3.0"),
+        .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.31.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftAPIGenerator",
-            dependencies: []),
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "SourceKittenFramework", package: "SourceKitten"),
+            ]),
         .testTarget(
             name: "SwiftAPIGeneratorTests",
             dependencies: ["SwiftAPIGenerator"]),
